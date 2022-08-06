6552c0fc52164c1bab2502cd153da8084fae3c52
dante <dantehduran@gmail.com>
2022-08-05 20:07:33 -0500

    dashboard aside

--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,11 +1,18 @@
 <template>
-	<div class="flex flex-col gap-6 p-4">
-		<header>
-			<h1 class="text-3xl font-semibold leading-loose text-white">Dashboard</h1>
-			<div class="text-gray-300">Friday 5 Aug, 2022</div>
-		</header>
-		<hr class="border-gray-700" />
-		<StatsCard />
-		<OrderReport />
+	<div class="flex">
+		<div class="flex flex-col gap-6 p-4 flex-1">
+			<header>
+				<h1 class="text-3xl font-semibold leading-loose text-white">
+					Dashboard
+				</h1>
+				<div class="text-gray-300">Friday 5 Aug, 2022</div>
+			</header>
+			<hr class="border-gray-700" />
+			<StatsCard />
+			<OrderReport />
+		</div>
+		<aside class="flex flex-col gap-y-6 pt-6 pr-6 w-96">
+			<MostOrdered />
+		</aside>
 	</div>
 </template>
