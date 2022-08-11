fc1f0d4ed00477c6004283ea7d3d68a6e53871ff
dante <dantehduran@gmail.com>
2022-08-10 20:57:02 -0500

    sticky settings submenu

--- layouts/settings.vue
+++ layouts/settings.vue
@@ -8,26 +8,28 @@
 						Settings
 					</h1>
 				</header>
-				<div class="flex gap-x-6 flex-1">
-					<div class="bg-gray-900 flex flex-col rounded-lg">
-						<button
-							v-for="item in submenu"
-							class="flex py-6 px-8 gap-x-2 first:rounded-t-lg last:rounded-b-lg relative"
-							:class="
-								route.name.toString().split('-').at(-1).includes(item.name)
-									? 'bg-primary/20 text-primary after:w-1 after:h-10 after:bg-primary after:absolute after:rounded-full after:top-6 after:right-0'
-									: 'text-white'
-							"
-							@click="navigateTo('/settings' + item.path)"
-						>
-							<Icon :icon="item.icon" h="24" w="24" class="text-inherit" />
-							<div class="flex flex-col">
-								<span class="text-inherit text-start">{{ item.title }}</span>
-								<span class="text-gray-500 text-sm">{{ item.subtitle }}</span>
-							</div>
-						</button>
+				<div class="flex gap-x-6">
+					<div class="">
+						<div class="flex flex-col rounded-lg sticky top-4">
+							<button
+								v-for="item in submenu"
+								class="flex py-6 px-8 gap-x-2 first:rounded-t-lg last:rounded-b-lg relative"
+								:class="
+									route.name.toString().split('-').at(-1).includes(item.name)
+										? 'bg-primary/20 text-primary after:w-1 after:h-10 after:bg-primary after:absolute after:rounded-full after:top-6 after:right-0'
+										: 'text-white bg-gray-900'
+								"
+								@click="navigateTo('/settings' + item.path)"
+							>
+								<Icon :icon="item.icon" h="24" w="24" class="text-inherit" />
+								<div class="flex flex-col">
+									<span class="text-inherit text-start">{{ item.title }}</span>
+									<span class="text-gray-500 text-sm">{{ item.subtitle }}</span>
+								</div>
+							</button>
+						</div>
 					</div>
-					<div class="bg-gray-900 rounded-lg"><slot /></div>
+					<div class="bg-gray-900 rounded-lg flex-1"><slot /></div>
 				</div>
 			</div>
 		</main>
--- pages/settings/products.vue
+++ pages/settings/products.vue
@@ -1,4 +1,55 @@
 <template>
-	<div>yoyo</div>
+	<div class="flex flex-col gap-y-4 p-6">
+		<div class="flex justify-between items-center pb-4">
+			<h2 class="text-xl font-semibold leading-loose text-white">
+				Product Management
+			</h2>
+			<button
+				class="flex py-3 px-4 rounded-lg border border-gray-700 gap-x-2.5"
+			>
+				<Icon
+					icon="fluent:options-20-regular"
+					h="20"
+					w="20"
+					class="text-white"
+				/>
+				<span class="text-sm text-white">Manage Categories</span>
+			</button>
+		</div>
+		<div class="flex gap-x-6 items-center border-b border-gray-700">
+			<button
+				v-for="category in categories"
+				@click="active = category"
+				class="capitalize font-medium pb-2"
+				:class="
+					active === category
+						? 'text-primary border-b-2 border-primary'
+						: 'text-white'
+				"
+			>
+				{{ category }}
+			</button>
+		</div>
+		<div class="grid grid-cols-4 gap-2">
+			<AddNew />
+			<AddNew />
+			<AddNew />
+			<AddNew />
+			<AddNew />
+			<AddNew />
+			<AddNew />
+			<AddNew />
+		</div>
+	</div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const active = ref('hot dishes');
+const categories = [
+	'hot dishes',
+	'cold dishes',
+	'soup',
+	'grill',
+	'appetizer',
+	'dessert',
+];
+</script>
