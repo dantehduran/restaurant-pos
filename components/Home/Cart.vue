959adbf2dcfefb25a32efa4e829f4e11c30a4db3
dante <dantehduran@gmail.com>
2022-08-19 16:35:03 -0500

    cart items section with hiden scrollbar

--- components/Home/Cart.vue
+++ components/Home/Cart.vue
@@ -1,44 +1,52 @@
 <template>
 	<div class="sticky bg-gray-900 rounded-l-lg top-0 min-h-screen">
-		<div class="flex flex-col gap-y-4 pt-6 px-4">
-			<h3 class="text-white text-xl">Orders #312</h3>
-			<div class="flex gap-x-2">
+		<div class="flex flex-col gap-y-4 px-4 justify-between max-h-screen">
+			<div class="bg-gray-900 flex flex-col gap-y-4 pt-6">
+				<h3 class="text-white text-xl">Orders #312</h3>
+				<div class="flex gap-x-2">
+					<button
+						@click="active = option"
+						v-for="option in options"
+						class="py-2 px-3 rounded-lg text-sm capitalize font-semibold border border-gray-600"
+						:class="
+							active === option
+								? 'bg-primary text-white'
+								: 'text-primary bg-inherit'
+						"
+					>
+						{{ option }}
+					</button>
+				</div>
+				<div class="flex gap-x-4 py-2 mb-2 border-b border-gray-600">
+					<span class="flex-1 text-lg text-white">Item</span>
+					<span class="text-lg text-white">Qty</span>
+					<span class="text-lg text-white px-2">Price</span>
+				</div>
+			</div>
+			<div
+				class="flex flex-col h-1/3 max-h-[50%] gap-y-2 overflow-y-auto scrollbar-hide"
+			>
+				<HomeCartItem />
+				<HomeCartItem />
+				<HomeCartItem />
+				<HomeCartItem />
+				<HomeCartItem />
+			</div>
+			<div class="bg-gray-900 flex flex-col gap-y-4 py-4">
+				<div class="flex justify-between items-center">
+					<span class="text-sm text-gray-600">Discount</span>
+					<span class="text-sm text-white text-right">$ 0</span>
+				</div>
+				<div class="flex justify-between items-center">
+					<span class="text-sm text-gray-600">Sub total</span>
+					<span class="text-sm text-white text-right">$ 21,03</span>
+				</div>
 				<button
-					@click="active = option"
-					v-for="option in options"
-					class="py-2 px-3 rounded-lg text-sm capitalize font-semibold border border-gray-600"
-					:class="
-						active === option
-							? 'bg-primary text-white'
-							: 'text-primary bg-inherit'
-					"
+					class="w-full text-center py-3 bg-primary text-white rounded-lg shadow-primary"
 				>
-					{{ option }}
+					Continue to Payment
 				</button>
 			</div>
-			<div class="flex gap-x-4 py-2 mb-2 border-b border-gray-600">
-				<span class="flex-1 text-lg text-white">Item</span>
-				<span class="text-lg text-white">Qty</span>
-				<span class="text-lg text-white px-2">Price</span>
-			</div>
-			<div class="flex flex-col gap-y-2">
-				<HomeCartItem />
-				<HomeCartItem />
-				<HomeCartItem />
-			</div>
-			<div class="flex justify-between items-center">
-				<span class="text-sm text-gray-600">Discount</span>
-				<span class="text-sm text-white text-right">$ 0</span>
-			</div>
-			<div class="flex justify-between items-center">
-				<span class="text-sm text-gray-600">Sub total</span>
-				<span class="text-sm text-white text-right">$ 21,03</span>
-			</div>
-			<button
-				class="w-full text-center py-3 bg-primary text-white rounded-lg shadow-primary"
-			>
-				Continue to Payment
-			</button>
 		</div>
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
 	content: [
 		`components/**/*.{vue,js}`,
@@ -24,5 +25,20 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function ({ addUtilities }) {
+			addUtilities({
+				'.scrollbar-hide': {
+					/* IE and Edge */
+					'-ms-overflow-style': 'none',
+					/* Firefox */
+					'scrollbar-width': 'none',
+					/* Safari and Chrome */
+					'&::-webkit-scrollbar': {
+						display: 'none',
+					},
+				},
+			});
+		}),
+	],
 };
