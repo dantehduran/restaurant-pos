cfa6c7a943a4fe5bd85d99ebc4d8153aba6270c8
dante <dantehduran@gmail.com>
2022-08-24 16:57:04 -0500

    redesign cart

--- components/Home/Cart.vue
+++ components/Home/Cart.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="sticky bg-gray-900 rounded-l-lg top-0 min-h-screen">
-		<div class="flex flex-col gap-y-4 px-4 justify-between max-h-screen">
+		<div class="flex flex-col gap-y-4 px-4 max-h-screen min-h-screen">
 			<div class="bg-gray-900 flex flex-col gap-y-4 pt-6">
-				<h3 class="text-white text-xl">Orders #312</h3>
+				<h3 class="text-white text-xl">Table 4</h3>
 				<div class="flex gap-x-2">
 					<button
 						@click="active = option"
@@ -17,24 +17,21 @@
 						{{ option }}
 					</button>
 				</div>
-				<div class="flex gap-x-4 py-2 mb-2 border-b border-gray-600">
-					<span class="flex-1 text-lg text-white">Item</span>
-					<span class="text-lg text-white">Qty</span>
-					<span class="text-lg text-white px-2">Price</span>
-				</div>
 			</div>
 			<div
-				class="flex flex-col h-1/3 max-h-[50%] gap-y-2 overflow-y-auto scrollbar-hide"
+				class="flex flex-col max-h-[50%] gap-y-2 overflow-y-auto scrollbar-hide"
 			>
 				<HomeCartItem />
 				<HomeCartItem />
 				<HomeCartItem />
+				<!-- <HomeCartItem />
 				<HomeCartItem />
 				<HomeCartItem />
+				<HomeCartItem /> -->
 			</div>
-			<div class="bg-gray-900 flex flex-col gap-y-4 py-4">
+			<div class="bg-gray-900 flex flex-col gap-y-4 py-4 mt-auto">
 				<div class="flex justify-between items-center">
-					<span class="text-sm text-gray-600">Discount</span>
+					<span class="text-sm text-gray-600">Tax</span>
 					<span class="text-sm text-white text-right">$ 0</span>
 				</div>
 				<div class="flex justify-between items-center">
@@ -44,7 +41,7 @@
 				<button
 					class="w-full text-center py-3 bg-primary text-white rounded-lg shadow-primary"
 				>
-					Continue to Payment
+					Place Order
 				</button>
 			</div>
 		</div>
